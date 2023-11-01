f4a3f1d8a1a3c66efef9a15465a458f1ea3f10f2
imall <imall841110@gmail.com>
2023-11-01 12:11:40 +0800

    initial commit

---
+++ Leetcode-easy/501. Find Mode in Binary Search Tree.js
@@ -0,0 +1,64 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function findMode(root) {
+    console.log(root)
+    const map = new Map();
+    let maxCount = 0;
+    const modes = []; 
+
+
+    function inOrder(node) {
+        if (node) {
+            inOrder(node.left);
+            const count = (map.get(node.val) || 0) + 1;
+            map.set(node.val, count);
+            maxCount = Math.max(maxCount, count);
+
+            inOrder(node.right);
+        }
+    }
+
+    inOrder(root);
+
+    for (const [key, value] of map) {
+        if (value === maxCount) {
+            modes.push(key);
+        }
+    }
+   
+    return modes;
+}
+
+
+// 創建測試用的二叉搜索樹
+const root1 = new TreeNode(1);
+root1.right = new TreeNode(2);
+root1.right.left = new TreeNode(2);
+
+console.log(findMode(root1)); // 輸出 [2]
+
+const root2 = new TreeNode(1);
+root2.left = new TreeNode(1);
+root2.right = new TreeNode(2);
+root2.right.left = new TreeNode(2);
+
+console.log(findMode(root2)); // 輸出 [1, 2]
+
+const root3 = new TreeNode(3);
+root3.left = new TreeNode(1);
+root3.right = new TreeNode(5);
+root3.left.right = new TreeNode(3);
+root3.right.left = new TreeNode(5);
+
+console.log(findMode(root3)); // 輸出 [3, 5]
+
+const root4 = new TreeNode(1);
+root4.right = new TreeNode(1);
+
+console.log(findMode(root4)); // 輸出 [1]
